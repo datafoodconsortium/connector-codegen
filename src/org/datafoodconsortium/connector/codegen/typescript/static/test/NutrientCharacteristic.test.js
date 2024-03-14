9c4275b5ff317a518859c3c79e01243345d52643
Maxime Lecoq <maxime@lecoqlibre.fr>
2024-03-14 22:14:15 +0100

    Fix mistaken expected json for some tests

--- src/org/datafoodconsortium/connector/codegen/typescript/static/test/NutrientCharacteristic.test.js
+++ src/org/datafoodconsortium/connector/codegen/typescript/static/test/NutrientCharacteristic.test.js
@@ -22,7 +22,7 @@ const nutrientCharacteristic = new NutrientCharacteristic({
     nutrientDimension: nutrientDimension
 });
 
-const json = `{"@context":"https://www.datafoodconsortium.org","@id":"_:b1","@type":"dfc-b:NutrientCharacteristic","dfc-b:hasNutrientDimension":{"@id":"dfc-m:Calcium"},"dfc-b:hasUnit":"dfc-m:Kilogram","dfc-b:value":"10"}`;
+const json = `{"@context":"https://www.datafoodconsortium.org","@id":"_:b1","@type":"dfc-b:NutrientCharacteristic","dfc-b:hasNutrientDimension":"dfc-m:Calcium","dfc-b:hasUnit":"dfc-m:Kilogram","dfc-b:value":"10"}`;
 
 test('NutrientCharacteristic:import', async () => {
     const imported = await connector.import(json);
