1cb3083ef5c2ac806ae394e7781bdcb9e166d836
Jamie Gaehring <gaehj457@gmail.com>
2024-04-01 19:06:53 -0400

    Generate TS tests for concrete class constructors.

---
+++ src/org/datafoodconsortium/connector/codegen/typescript/static/test/utils.js
@@ -0,0 +1,19 @@
+import { ok, AssertionError } from 'node:assert';
+import { SemanticObject } from '@virtual-assembly/semantizer';
+
+/**
+ * assertSemanticEqual
+ * @param {*} actual Actual test value
+ * @param {SemanticObject} expected Expected test value
+ */
+export function assertSemanticEqual(actual, expected) {
+	const operator = 'SemanticObject.equals';
+	const errOpts = { actual, expected, operator };
+	if (!(actual instanceof SemanticObject)) {
+		errOpts.message = 'Expected an instance of SemanticObject:';
+	} else if (!actual.equals(expected)) {
+		errOpts.message = 'Expected semantic objects to have equal properties:';
+	}
+	if (errOpts.message) throw new AssertionError(errOpts);
+	ok(actual);
+}
