e630d0334b70e4a7e7e07a9151fecaf53af0557e
Maxime Lecoq <maxime@lecoqlibre.fr>
2024-10-22 13:37:30 +0200

    Release v1.1.0
    Squashed commit of the following:
    
    commit f557f8ac062b653e68d77830ce3fff5e1b5e719d
    Author: Maxime Lecoq <maxime@lecoqlibre.fr>
    Date:   Tue Oct 15 10:34:05 2024 +0200
    
        Update main changelog, fix typos in Ruby changelog
    
    commit d35da0b5288f77628eddee122341088ade1085a1
    Merge: 4f8af26 9d28313
    Author: Maxime Lecoq-Gaillard <maxime@lecoqlibre.fr>
    Date:   Tue Oct 15 10:08:07 2024 +0200
    
        Merge pull request #23 from mkllnk:ruby-releases
    
        Merge ruby releases into v1.1.0 branch
    
    commit 9d28313d0242cba75cd73057c9e2c57588a48d37
    Merge: 2f43f8e 4f8af26
    Author: Maxime Lecoq-Gaillard <maxime@lecoqlibre.fr>
    Date:   Tue Oct 15 10:06:34 2024 +0200
    
        Merge branch 'v1.1.0' into ruby-releases
    
    commit 2f43f8e7e731285aaf531a80440af3b7a1bfa956
    Merge: b751f25 ff68686
    Author: Maxime Lecoq-Gaillard <maxime@lecoqlibre.fr>
    Date:   Tue Oct 15 09:55:27 2024 +0200
    
        Merge pull request #23 from mkllnk/ruby-releases
    
        Ruby releases
    
    commit ff6868654d8866852282f4ff3d43cd967f2b90bb
    Author: Maikel Linke <maikel@email.org.au>
    Date:   Fri Oct 11 08:53:50 2024 +1100
    
        Release v1.0.0-alpha.13
    
    commit c3bc0728a936106b79dfba61c6bf40bfe3934aaf
    Author: Maikel Linke <maikel@email.org.au>
    Date:   Fri Oct 11 08:39:33 2024 +1100
    
        Clarify nil values in Changelog
    
    commit e22a5eafd6e336a8d11e9281838b92c91dd558dc
    Author: Maikel Linke <maikel@email.org.au>
    Date:   Thu Apr 11 08:32:36 2024 +1000
    
        Add category to latest release notes
    
    commit afa9a4301dcd09cac68c97a5c3b90ce0eaa5929c
    Author: Maikel Linke <maikel@email.org.au>
    Date:   Thu Apr 11 08:15:56 2024 +1000
    
        Release v1.0.0-alpha.12
    
    commit a6e3e990fb8fa6abd946d5c4725839ded928b98b
    Author: Maikel Linke <maikel@email.org.au>
    Date:   Thu Apr 11 08:00:48 2024 +1000
    
        Ruby: Bump semantizer from 1.0.5 to 1.1.1
    
    commit 730d9cb77d16914391c018529adc62e28cc446fe
    Author: Maikel Linke <maikel@email.org.au>
    Date:   Thu Apr 4 16:47:46 2024 +1100
    
        Release 1.0.0.pre.alpha.11
    
    commit 91908313e7a33eda60c0607fd1575c3394141c2b
    Author: Maikel Linke <maikel@email.org.au>
    Date:   Thu Apr 4 16:01:49 2024 +1100
    
        Ruby: generate with data model next branch
    
        We need the updated data model in openfoodnetwork.
    
        Also updated specs to compare hashes instead of strings which produces
        better output and is easier to maintain.
    
        Then applied some layout formatting with rubocop on spec files.
    
    commit 95503224e4d15cba19de0f5f4492fc01dac63531
    Author: Maikel Linke <maikel@email.org.au>
    Date:   Thu Apr 4 14:31:05 2024 +1100
    
        Use RSpec recommended config
    
    commit 4158dacfeecd70a4c996f18dfb19bc53aebc1b8e
    Author: Maikel Linke <maikel@email.org.au>
    Date:   Thu Apr 4 14:13:20 2024 +1100
    
        Bump rspec from 3.12.0 to 3.13.0
    
    commit 2c338cf4eef386a3fbdf17d9bdad7326191d540d
    Author: Maikel Linke <maikel@email.org.au>
    Date:   Tue Jan 30 15:34:59 2024 +1100
    
        Release 1.0.0-alpha.10
    
    commit 4f8af26d53602c8b6a3ed1e8891462271c9a2fd1
    Author: Maikel <maikel@email.org.au>
    Date:   Fri Apr 12 20:18:04 2024 +1000
    
        Ruby: Release 1.0.0-alpha.10 and 1.0.0-alpha.11 (#21)
    
        * Release 1.0.0-alpha.10
    
        * Bump rspec from 3.12.0 to 3.13.0
    
        * Use RSpec recommended config
    
        * Ruby: generate with data model next branch
    
        We need the updated data model in openfoodnetwork.
    
        Also updated specs to compare hashes instead of strings which produces
        better output and is easier to maintain.
    
        Then applied some layout formatting with rubocop on spec files.
    
        * Release 1.0.0.pre.alpha.11
    
        * Ruby: Bump semantizer from 1.0.5 to 1.1.1
    
        * Release v1.0.0-alpha.12
    
        * Add category to latest release notes

--- src/org/datafoodconsortium/connector/codegen/ruby/static/spec/enterprise_spec.rb
+++ src/org/datafoodconsortium/connector/codegen/ruby/static/spec/enterprise_spec.rb
@@ -1,60 +1,84 @@
-describe DataFoodConsortium::Connector::Enterprise do
+RSpec.describe DataFoodConsortium::Connector::Enterprise do
   it "can be empty when exported" do
     subject = DataFoodConsortium::Connector::Enterprise.new(
       "https://myplatform.com/e"
     )
-    result = connector.export(subject)
+    result = exported_json(subject)
     expect(result).to eq(
-      '{"@context":"https://www.datafoodconsortium.org","@id":"https://myplatform.com/e","@type":"dfc-b:Enterprise"}'
+      {
+        "@context" => "https://www.datafoodconsortium.org",
+        "@id" => "https://myplatform.com/e",
+        "@type" => "dfc-b:Enterprise",
+      }
     )
   end
 
   it "contains all fields when exported" do
     subject = DataFoodConsortium::Connector::Enterprise.new(
-        "https://myplatform.com/e",
-        name: "name", 
-        description: "description", 
-        vatNumber: "vatNumber", 
-        customerCategories: [
-            DataFoodConsortium::Connector::CustomerCategory.new("https://myplatform.com/cc")
-        ], 
-        suppliedProducts: [
-            DataFoodConsortium::Connector::SuppliedProduct.new("https://myplatform.com/sp")
-        ], 
-        catalogItems: [
-            DataFoodConsortium::Connector::CatalogItem.new("https://myplatform.com/ci")
-        ], 
-        localizations: []
+      "https://myplatform.com/e",
+      name: "name",
+      description: "description",
+      vatNumber: "vatNumber",
+      customerCategories: [
+        DataFoodConsortium::Connector::CustomerCategory.new("https://myplatform.com/cc")
+      ],
+      suppliedProducts: [
+        DataFoodConsortium::Connector::SuppliedProduct.new("https://myplatform.com/sp")
+      ],
+      catalogItems: [
+        DataFoodConsortium::Connector::CatalogItem.new("https://myplatform.com/ci")
+      ],
+      localizations: []
     )
-    result = connector.export(subject)
+    result = exported_json(subject)
     expect(result).to eq(
-      '{"@context":"https://www.datafoodconsortium.org","@id":"https://myplatform.com/e","@type":"dfc-b:Enterprise","dfc-b:name":"name","dfc-b:hasDescription":"description","dfc-b:VATnumber":"vatNumber","dfc-b:defines":"https://myplatform.com/cc","dfc-b:manages":"https://myplatform.com/ci","dfc-b:supplies":"https://myplatform.com/sp"}'
+      {
+        "@context" => "https://www.datafoodconsortium.org",
+        "@id" => "https://myplatform.com/e",
+        "@type" => "dfc-b:Enterprise",
+        "dfc-b:VATnumber" => "vatNumber",
+        "dfc-b:defines" => "https://myplatform.com/cc",
+        "dfc-b:hasDescription" => "description",
+        "dfc-b:manages" => "https://myplatform.com/ci",
+        "dfc-b:name" => "name",
+        "dfc-b:supplies" => "https://myplatform.com/sp",
+      }
     )
   end
 
   it "contains collections as arrays when exported" do
     subject = DataFoodConsortium::Connector::Enterprise.new(
-        "https://myplatform.com/e",
-        name: "name", 
-        description: "description", 
-        vatNumber: "vatNumber", 
-        customerCategories: [
-            DataFoodConsortium::Connector::CustomerCategory.new("https://myplatform.com/cc"),
-            DataFoodConsortium::Connector::CustomerCategory.new("https://myplatform.com/cc2")
-        ], 
-        suppliedProducts: [
-            DataFoodConsortium::Connector::SuppliedProduct.new("https://myplatform.com/sp"),
-            DataFoodConsortium::Connector::SuppliedProduct.new("https://myplatform.com/sp2")
-        ], 
-        catalogItems: [
-            DataFoodConsortium::Connector::CatalogItem.new("https://myplatform.com/ci"),
-            DataFoodConsortium::Connector::CatalogItem.new("https://myplatform.com/ci2")
-        ], 
-        localizations: []
+      "https://myplatform.com/e",
+      name: "name",
+      description: "description",
+      vatNumber: "vatNumber",
+      customerCategories: [
+        DataFoodConsortium::Connector::CustomerCategory.new("https://myplatform.com/cc"),
+        DataFoodConsortium::Connector::CustomerCategory.new("https://myplatform.com/cc2")
+      ],
+      suppliedProducts: [
+        DataFoodConsortium::Connector::SuppliedProduct.new("https://myplatform.com/sp"),
+        DataFoodConsortium::Connector::SuppliedProduct.new("https://myplatform.com/sp2")
+      ],
+      catalogItems: [
+        DataFoodConsortium::Connector::CatalogItem.new("https://myplatform.com/ci"),
+        DataFoodConsortium::Connector::CatalogItem.new("https://myplatform.com/ci2")
+      ],
+      localizations: []
     )
-    result = connector.export(subject)
+    result = exported_json(subject)
     expect(result).to eq(
-      '{"@context":"https://www.datafoodconsortium.org","@id":"https://myplatform.com/e","@type":"dfc-b:Enterprise","dfc-b:name":"name","dfc-b:hasDescription":"description","dfc-b:VATnumber":"vatNumber","dfc-b:defines":["https://myplatform.com/cc","https://myplatform.com/cc2"],"dfc-b:manages":["https://myplatform.com/ci","https://myplatform.com/ci2"],"dfc-b:supplies":["https://myplatform.com/sp","https://myplatform.com/sp2"]}'
+      {
+        "@context" => "https://www.datafoodconsortium.org",
+        "@id" => "https://myplatform.com/e",
+        "@type" => "dfc-b:Enterprise",
+        "dfc-b:VATnumber" => "vatNumber",
+        "dfc-b:defines" => ["https://myplatform.com/cc", "https://myplatform.com/cc2"],
+        "dfc-b:hasDescription" => "description",
+        "dfc-b:manages" => ["https://myplatform.com/ci", "https://myplatform.com/ci2"],
+        "dfc-b:name" => "name",
+        "dfc-b:supplies" => ["https://myplatform.com/sp", "https://myplatform.com/sp2"],
+      }
     )
   end
 end
