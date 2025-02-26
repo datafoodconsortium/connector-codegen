c14853551c061386dce9b6d1839ad852de2676c4
Jamie Gaehring <jamie@runrig.org>
2025-02-25 21:38:25 -0500

    Fix TS setters for array properties.

--- src/org/datafoodconsortium/connector/codegen/typescript/static/src/IConnectorStore.ts
+++ src/org/datafoodconsortium/connector/codegen/typescript/static/src/IConnectorStore.ts
@@ -4,6 +4,7 @@ import { Semanticable } from "@virtual-assembly/semantizer"
 export default interface IConnectorStore {
     get(semanticObjectId: string): Promise<Semanticable | undefined>;
     has(semanticObjectId: string): boolean;
+    remove(semanticObjectId: string): void;
     set(semanticObject: Semanticable): void;
     setAll(semanticObjects: Array<Semanticable>): void;
 }
