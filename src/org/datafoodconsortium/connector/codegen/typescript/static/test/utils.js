bb35e7ebc20fba5edb2f453a2a69890e533262b6
Jamie Gaehring <jamie@runrig.org>
2025-03-23 10:49:00 -0400

    Create a TestObserver utility for TS tests.

--- src/org/datafoodconsortium/connector/codegen/typescript/static/test/utils.js
+++ src/org/datafoodconsortium/connector/codegen/typescript/static/test/utils.js
@@ -1,4 +1,4 @@
-import { ok, AssertionError } from 'node:assert';
+import { equal, ok, AssertionError } from 'node:assert';
 import { SemanticObject } from '@virtual-assembly/semantizer';
 
 /**
@@ -16,4 +16,53 @@ export function assertSemanticEqual(actual, expected) {
 	}
 	if (errOpts.message) throw new AssertionError(errOpts);
 	ok(actual);
+}
+
+/**
+ * @import { TestContext } from 'node:test
+ * @import { Observer } from "../src/observer"
+ * @template T
+ * @implements { Observer<T> }
+ */
+export class TestObserver {
+	/** @public {T} */
+	expected;
+	/** @public {(actual: unknown, expected: T) => void} */
+	assert = equal;
+	/** @public {(result?: any) => void} */
+	done;
+
+	/**
+	 * @constructor
+	 * @param {T} expected
+	 * @param {(actual: unknown, expected: T) => void} [assert=equal]
+	 * @param {(result?: any) => void} [done]
+	 */
+	constructor(expected, assert = equal, done = undefined) {
+		this.expected = expected;
+		if (typeof assert === 'function') this.assert = assert;
+		if (typeof done === 'function') this.done = done;
+	}
+
+	next(actual) {
+		if (typeof this.assert !== 'function' || this.assert.length < 2) {
+			const message =
+				`${this.assert} [TestObserver:assert] is not a valid assertion function.`;
+			throw new AssertionError({
+				actual, expected: this.expected, message, operator: 'TestObserver:next',
+			});
+		}
+		else this.assert(actual, this.expected);
+	}
+
+	error({ message }) {
+		const operator = 'TestObserver:error';
+		throw new AssertionError({ message, operator, expected: this.expected });
+	}
+
+	complete() {
+		ok(true, 'TestObserver:complete');
+		if (typeof this.done === 'function') this.done();
+	}
+
 }
